cce7ecb808cd484c0ec47e2948c0682eb67c1461
zhoulimeng <zhoulimeng@bz365.com>
2019-01-30 16:24:26 +0800

    加载异步路由

--- src/main.js
+++ src/main.js
@@ -5,8 +5,8 @@ import App from './App'
 import VueRouter from 'vue-router'
 import Vuex from 'vuex'
 // import VueResource from 'vue-resource'
-import home from './components/home/home'
-import category from './components/category/category'
+// import home from './components/home/home'
+// import category from './components/category/category'
 import cart from './components/cart/cart'
 import order from './components/order/order'
 import detail from './components/detail/detail'
@@ -22,9 +22,10 @@ Vue.config.productionTip = false
 var router = new VueRouter({
   mode: 'history',
   routes: [
-    { path: '/', component: home },
-    { path: '/home', component: home },
-    { path: '/category', component: category },
+    { path: '/', component: () => import(/* webpackChunkName: 'home' */'./components/home/home') },
+    { path: '/home', component: () => import(/* webpackChunkName: 'home' */'./components/home/home') },
+    /* webpackChunkName: 'category' 是异步加载生成文件自定义的名称 */
+    { path: '/category', component: () => import(/* webpackChunkName: 'category' */'./components/category/category') },
     { path: '/cart', component: cart },
     { path: '/order', component: order },
     { path: '/detail/', name: 'detail', component: detail }

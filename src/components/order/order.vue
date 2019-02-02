8554536b18039628cf9d225e9cb7f33615a437c7
lemonzhou <zhoulimeng@bz365.com>
2019-02-02 10:18:10 +0800

    order

--- src/components/order/order.vue
+++ src/components/order/order.vue
@@ -1,5 +1,31 @@
 <template>
-    <div>
-        order
+    <div class="order">
+        <div class="topHeader">
+            <img src="@/components/detail/132.jpeg" alt="">
+        </div>
     </div>
 </template>
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.order {
+    width: 100%;
+    background: #f7f7f7;
+}
+.topHeader {
+    height: 150px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+.topHeader img {
+    width: 60px;
+    height: 60px;
+    border-radius: 100%;
+}
+</style>
+
